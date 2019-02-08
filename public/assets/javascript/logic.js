d537de5c8669686914420690e254e659ece0ac30
Cjay1019 <cjay741@gmail.com>
2019-02-08 13:54:29 -0500

    fully finished api route logic and front end jquery, app fully functions

--- public/assets/javascript/logic.js
+++ public/assets/javascript/logic.js
@@ -47,6 +47,27 @@ $("#submit-btn").on("click", function(event) {
     ]
   };
   $.post("/api/newmemer", newMemer).then(function(data) {
-    console.log(data);
+    switch (data) {
+      case "First":
+        $("#modalTitle").text("You're The First Memer");
+        $("#modalBody").text("Check back later to find your meme match!");
+        $("#modalBody").append(
+          "<img src='https://media.tenor.com/images/1200075e7ad907ee57e6b70f500bce57/tenor.gif' class='img-fluid'>"
+        );
+        break;
+      case "Exists":
+        $("#modalTitle").text("This Memer Already Exists");
+        $("#modalBody").text("Try entering a different name!");
+        $("#modalBody").append(
+          "<img src='https://media.tenor.com/images/1200075e7ad907ee57e6b70f500bce57/tenor.gif' class='img-fluid'>"
+        );
+        break;
+      default:
+        $("#modalTitle").text("Your Meme Match");
+        $("#modalBody").text(data.name + " has the most similar meme taste!");
+        $("#modalBody").append(
+          "<img src='" + data.photo + "' class='img-fluid'>"
+        );
+    }
   });
 });
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var memers = require("../app/data/friends");
+var memers = require("../app/data/memers");
 
 module.exports = function(app) {
   app.get("/api/memers", function(req, res) {
@@ -6,7 +6,29 @@ module.exports = function(app) {
   });
 
   app.post("/api/newmemer", function(req, res) {
+    var matchValues = [];
+    if (memers.length === 0) {
+      res.json("First");
+      memers.push(req.body);
+      return;
+    } else {
+      for (i = 0; i < memers.length; i++) {
+        var matchValue = 0;
+        if (memers[i].name === req.body.name) {
+          res.json("Exists");
+          return;
+        } else {
+          for (j = 0; j < memers[i].scores.length; j++) {
+            var difference = memers[i].scores[j] - req.body.scores[j];
+            matchValue += Math.abs(difference);
+          }
+          matchValues.push(matchValue);
+        }
+        memers[i].match = matchValue;
+      }
+    }
+    var match = matchValues.indexOf(Math.min(...matchValues));
+    res.json(memers[match]);
     memers.push(req.body);
-    res.send(memers);
   });
 };
